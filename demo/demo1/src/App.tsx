d29d7ad66bc4322c58d49a56bb64242ec06d805b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-04 10:10:13 +0700

    1234

--- demo/demo1/src/App.tsx
+++ demo/demo1/src/App.tsx
@@ -3,19 +3,17 @@ import React from 'react'
 type Props = {}
 
 export default function App({}: Props) {
-  // SPA
+  // Typescript (Logic)
+  const tmp1 = 10
   
   // JSX
   return (
-    <div style={{paddingLeft: 40}}>
-      
-      React
+    <div style={{paddingLeft: 40}}>      
+      <h1>React</h1>
       <ul>
-        <li>xxx</li>
-        <li>xxx</li>
-        <li>xxx</li>
+        <li>Simple Text</li>
+        <li>Tmp1: {tmp1}</li>
       </ul>
-      
       </div>
   )
 }
