cc02dbb8bd2bd4fbff5f5bec3cba809c3fa0e494
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-04 15:22:17 +0700

    1234

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
+// yarn add @types/node -D
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
