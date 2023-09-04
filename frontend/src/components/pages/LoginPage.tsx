81345c9358bd0282aa933e0c8d5f75db140cce69
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-04 15:37:06 +0700

    1234

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 
 import cmLogo from "@/assets/images/authen_header.jpg";
+import { Box, Stack, TextField, Typography } from "@mui/material";
 
 type Props = {};
 
 export default function LoginPage({}: Props) {
   return (
-    <div>
-      LoginPage
-      <img src={cmLogo} alt="" className="w-[300px]" />
-    </div>
+    <Box>
+      <Typography variant="h2">Login</Typography>
+      <form className="mt-5">
+        <Stack direction={"column"} spacing={3}>
+          {/* Username */}
+          <TextField id="username" label="Username" variant="outlined" />
+
+          {/* Password */}
+          <TextField
+            id="passowrd"
+            label="Password"
+            variant="outlined"
+            type="password"
+          />
+        </Stack>
+      </form>
+    </Box>
   );
 }
