bfa428a52aab194ef1bf9ac9bc4f2dfe7e6fb045
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-04 11:39:31 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,5 +3,9 @@ import React from "react";
 type Props = {};
 
 export default function App({}: Props) {
-  return <div className="text-orange-500 bg-green-300 w-10">App</div>;
+  return (
+    <div className="text-orange-500 bg-green-300 w-[100px] h-[100px] text-3xl text-center m-5 p-5 rounded-md">
+      App
+    </div>
+  );
 }
