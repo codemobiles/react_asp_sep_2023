ae7d2481893df9f47a9110e16bdd2e6a6748d1cc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-05 15:17:06 +0700

    1234

--- backend/Installers/DBInstaller.cs
+++ backend/Installers/DBInstaller.cs
@@ -5,11 +5,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace backend.Installers
 {
+    
     public class DBInstaller : IInstaller
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
-           
+            services.AddDbContext<DatabaseContext>(option => option.UseSqlServer(
+                configuration.GetConnectionString("ConnectionSQLServer"))
+            );
         }
     }
 }
