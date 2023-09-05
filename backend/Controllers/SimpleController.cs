b32ecef799a8629b6107ca77c17c4e81d0a9fc52
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-05 11:05:05 +0700

    1234

--- backend/Controllers/SimpleController.cs
+++ backend/Controllers/SimpleController.cs
@@ -18,8 +18,33 @@ namespace backend.Controllers
         [HttpGet("")]
         public ActionResult<IEnumerable<string>> Getstrings()
         {
-            return new List<string> { };
+            return new List<string> {"Angular", "Flutter", "ReactJS" };
         }
+
+        [HttpGet("courses")]
+        public ActionResult<IEnumerable<string>> GetCourses()
+        {
+            return new List<string> {"Flutter", "ReactJS", "1234" };
+        }
+
+        [HttpGet("method1")]
+        public ActionResult<string> GetTModel()
+        {
+            return "method1 555";
+        }
+
+        [HttpGet("method2")]
+        public IActionResult GetMethod2()
+        {
+            return Ok(new List<string> {"Method2", "Flutter", "ReactJS", "1234" }) ;
+        }
+        
+        [HttpPost("login")]
+        public IActionResult Login(string model)
+        {
+            return Ok("ok");
+        }
+        
 
       
     }
