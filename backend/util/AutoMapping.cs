8a006a4daf46d998b12bafb9caa42ad7b38aee80
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-05 11:52:01 +0700

    1234

---
+++ backend/util/AutoMapping.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+using backend.Models;
+using backend.ViewModels;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        CreateMap<LoginViewModel, User>(); // map from UserViewModel to Users
+        // CreateMap<RegisterViewModel, User>(); // map from RegisterViewModel to Users
+    }
+}
