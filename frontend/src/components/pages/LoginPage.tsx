be3aa8f142c4a55ccff4348dbda77d4e0683f421
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 15:27:33 +0700

    1234

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,43 +1,39 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import {
-  Alert,
-  Box,
-  Button,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
+import loginBg from "@/assets/images/bg4.jpg";
+import { authSelector, login } from "@/store/slices/authSlice";
+import { useAppDispatch } from "@/store/store";
 import { User } from "@/types/user.type";
-import { Controller, useForm } from "react-hook-form";
-import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { httpClient } from "@/utils/HttpClient";
+import * as Icons from "@mui/icons-material/";
+import { Box, InputAdornment } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { Controller, useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
-import { RootState, useAppDispatch } from "@/store/store";
-import {
-  add,
-  addWithDelay,
-  authSelector,
-  login,
-  remove,
-  removeWithDelay,
-} from "@/store/slices/authSlice";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 const formValidateSchema = Yup.object().shape({
-  username: Yup.string()
-    .required("Username must be more than 3 letters")
-    .min(4),
-  password: Yup.string().required("Password is required"),
+  // username: Yup.string().email("Invalid email address").required("Email is required").trim(),
+  username: Yup.string().min(4).required("Username must be more than 3 letters").trim(),
+  password: Yup.string().required("Password is required").trim(),
 });
 
-export default function LoginPage() {
-  const initialValue: User = { username: "admin", password: "12341234" };
-  const authReducer = useSelector(authSelector);
-  const dispatch = useAppDispatch();
+const Login = () => {
+  const dispatch = useAppDispatch(); // used to call actions
+  const authReducer = useSelector(authSelector); // used to access state
   const navigate = useNavigate();
 
+  const classes: any = {
+    root: { display: "flex", justifyContent: "center", alignItems: "center" },
+    submitBtn: { marginTop: 4 },
+    canelBtn: { marginTop: 2 },
+  };
+
+  const initialValue: User = { username: "admin", password: "1234" };
   const {
     control,
     handleSubmit,
@@ -47,89 +43,111 @@ export default function LoginPage() {
     resolver: yupResolver(formValidateSchema),
   });
 
-  const doSubmit = async (user: User) => {
-    const result = await dispatch(login(user));
+  const onSubmit = async (values: User) => {
+    const result = await dispatch(login(values));
     if (login.fulfilled.match(result)) {
       navigate("/stock");
     }
   };
 
-  return (
-    <Box>
-      <Typography variant="h2">Login</Typography>
-      <form className="mt-5" onSubmit={handleSubmit(doSubmit)}>
-        <Stack direction={"column"} spacing={3}>
-          {/* Username */}
-          <Controller
-            control={control}
-            name="username"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                error={Boolean(errors.username?.message)}
-                helperText={errors.username?.message}
-                id="username"
-                label="Username"
-                variant="outlined"
-              />
-            )}
-          />
-
-          {/* Password */}
-          <Controller
-            control={control}
-            name="password"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Password"
-                variant="outlined"
-                error={Boolean(errors.password?.message)}
-                helperText={errors.password?.message}
-              />
-            )}
-          />
-
-          {/* short if */}
-          {authReducer.isError && (
-            <Alert severity="error">
-              Login failed - please check your username and password
-            </Alert>
+  const showForm = () => {
+    return (
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              error={Boolean(errors.username?.message)}
+              helperText={errors.username?.message}
+              {...field}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Username"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Email />
+                  </InputAdornment>
+                ),
+              }}
+              autoComplete="email"
+              autoFocus
+            />
           )}
+        ></Controller>
 
-          <Button variant="contained" type="submit">
-            Login
-          </Button>
-          <Button variant="text">Don't have an account?</Button>
-          {/* Redux Counter */}
-          <Stack direction="row" justifyContent="space-around">
-            <Button variant="contained" onClick={() => dispatch(remove())}>
-              --
-            </Button>
-            <Typography variant="h2">{authReducer.count}</Typography>
-            <Button variant="contained" onClick={() => dispatch(add())}>
-              ++
-            </Button>
-          </Stack>
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              helperText={errors.password?.message}
+              error={Boolean(errors.password?.message)}
+              {...field}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icons.Password />
+                  </InputAdornment>
+                ),
+              }}
+              label="Password"
+              type="password"
+            />
+          )}
+        ></Controller>
 
-          {/* Redux Counter with Delay */}
-          <Stack direction="row" justifyContent="space-around">
-            <Button
-              variant="contained"
-              onClick={() => dispatch(removeWithDelay())}
-            >
-              delay(-)
-            </Button>
-            <Typography variant="h2">{authReducer.count}</Typography>
-            <Button
-              variant="contained"
-              onClick={() => dispatch(addWithDelay())}
-            >
-              delay(+)
-            </Button>
-          </Stack>
-        </Stack>
+        {authReducer.isError && <Alert severity="error">Login failed</Alert>}
+
+        <Button sx={classes.submitBtn} type="submit" fullWidth variant="contained" color="primary">
+          Login
+        </Button>
+
+        <Button
+          onClick={() => {
+            navigate("/register");
+          }}
+          type="button"
+          fullWidth
+          variant="outlined"
+          className="border-dashed border-1 border-gray-300 mt-4"
+          color="primary"
+        >
+          Register
+        </Button>
       </form>
+    );
+  };
+
+  return (
+    <Box className="flex justify-center items-center">
+      <Card className="max-w-[345px]">
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Login
+          </Typography>
+          {showForm()}
+        </CardContent>
+      </Card>
+      <style>
+        {`
+          body {
+            min-height: 100vh;
+            position: relative;
+            margin: 0;
+            background-size: cover;
+            background-image: url(${loginBg});
+            text-align: center;
+          }
+        `}
+      </style>
     </Box>
   );
-}
+};
+
+export default Login;
