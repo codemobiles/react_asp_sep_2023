8c2af57508d51df9303344650cc92a03c2ede9b8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 16:03:18 +0700

    1234

--- backend/Controllers/v1/ProductController.cs
+++ backend/Controllers/v1/ProductController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers.v1
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
