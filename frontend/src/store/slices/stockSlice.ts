64aa3542f0a9c2aff0827a22ec1e831b5d7d8d3b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 15:42:40 +0700

    1234

---
+++ frontend/src/store/slices/stockSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stockSlice = createSlice({
+    name:"stockSlice",
+    initialState:{},
+    reducers:{}
+})
+
+export default stockSlice.reducer
