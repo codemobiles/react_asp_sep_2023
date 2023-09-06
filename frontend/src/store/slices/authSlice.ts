8d8bc497dbd97cddfa343ad467f121231dade153
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 10:23:12 +0700

    1234

---
+++ frontend/src/store/slices/authSlice.ts
@@ -0,0 +1,11 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+const authSlice = createSlice({
+    name:"authSlice",
+    initialState:{count:0},
+    reducers:{},
+
+})
+
+export default authSlice.reducer
