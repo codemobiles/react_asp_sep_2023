74b83ba8bd94d87e0067210a840d2bcf9747e766
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 09:14:36 +0700

    1234

--- backend/ViewModels/RegisterViewModel.cs
+++ backend/ViewModels/RegisterViewModel.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
 namespace backend.Controllers.v1.ViewModels
 {
-    class RegisterViewModel
+    public class RegisterViewModel
     {
-        
+        [Required]
+        [MinLength(3, ErrorMessage = "username must have at least 3 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(10, ErrorMessage = "passwords must have at least 10 characters")]
+        public string Password { get; set; }
     }
     
 }
