d2b9acf03982a22bcf135438bcd1f80018919c53
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 13:39:11 +0700

    1234

--- frontend/src/store/slices/authSlice.ts
+++ frontend/src/store/slices/authSlice.ts
@@ -5,82 +5,91 @@ import { httpClient } from "@/utils/HttpClient";
 import { User } from "@/types/user.type";
 import { server } from "@/utils/constants";
 
-
 export interface AuthState {
-    loginResult?: LoginResult;
-    registerResult?: RegisterResult;
-    isAuthenticating: boolean;
-    isAuthented: boolean;
-    isError: boolean;
-    count: number;
-  }
-  
-  const initialState: AuthState = {
-    isAuthenticating: true,
-    isAuthented: false,
-    isError: false,
-    count:0
-  };
+  loginResult?: LoginResult;
+  registerResult?: RegisterResult;
+  isAuthenticating: boolean;
+  isAuthented: boolean;
+  isError: boolean;
+  count: number;
+}
 
-  export const addWithDelay = createAsyncThunk("addWithDelay", async ()=>{
-    // get result from server (delay 1 secs)
-    await new Promise(resolve => setTimeout(resolve, 1000))    
-    return getStore().getState().authReducer.count + 1
-  })
+const initialState: AuthState = {
+  isAuthenticating: true,
+  isAuthented: false,
+  isError: false,
+  count: 0,
+};
 
-  export const removeWithDelay = createAsyncThunk("removeWithDelay", async ()=>{
-    // get result from server (delay 1 secs)
-    await new Promise(resolve => setTimeout(resolve, 1000))    
-    return getStore().getState().authReducer.count - 1
-  })
+export const addWithDelay = createAsyncThunk("addWithDelay", async () => {
+  // get result from server (delay 1 secs)
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return getStore().getState().authReducer.count + 1;
+});
 
+export const removeWithDelay = createAsyncThunk("removeWithDelay", async () => {
+  // get result from server (delay 1 secs)
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return getStore().getState().authReducer.count - 1;
+});
 
 export const login = createAsyncThunk("auth/login", async (value: User) => {
-    const result = await httpClient.post<LoginResult>(server.LOGIN_URL, value);
-    if (result.data.result === "ok") {
-      const { token } = result.data;
-      localStorage.setItem(server.TOKEN_KEY, token);
-      return result.data;
-    }
-    throw Error();
-  });
-  
-  export const register = createAsyncThunk("auth/register", async (value: User) => {
-    const result = await httpClient.post<RegisterResult>(server.REGISTER_URL, value);
-    if (result.data.result === "ok") {
-      return result.data;
-    }
-  
-    throw Error();
-  });
+  const result = await httpClient.post<LoginResult>(server.LOGIN_URL, value);
+  if (result.data.result === "ok") {
+    const { token } = result.data;
+    localStorage.setItem(server.TOKEN_KEY, token);
+    return result.data;
+  }
+  throw Error();
+});
 
-  
+export const register = createAsyncThunk(
+  "auth/register",
+  async (value: User) => {
+    const result = await httpClient.post<RegisterResult>(
+      server.REGISTER_URL,
+      value
+    );
+    if (result.data.result === "ok") {
+      return result.data;
+    }
+
+    throw Error();
+  }
+);
 
 const authSlice = createSlice({
-    name:"authSlice",
-    initialState,
-    reducers:{
-        add:(state)=>{
-            state.count++
-        },
-        remove:(state)=>{
-            state.count--
-        },
-        logout:()=>{
-            localStorage.clear()
-        }        
+  name: "authSlice",
+  initialState,
+  reducers: {
+    add: (state) => {
+      state.count++;
     },
-    extraReducers:(builder)=>{
-        builder.addCase(addWithDelay.fulfilled, (state, action)=>{
-            state.count = action.payload
-        })
+    remove: (state) => {
+      state.count--;
+    },
+    logout: () => {
+      localStorage.clear();
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addWithDelay.fulfilled, (state, action) => {
+      state.count = action.payload;
+    });
 
-        builder.addCase(removeWithDelay.fulfilled, (state, action)=>{
-            state.count = action.payload
-        })
-    }
-})
+    builder.addCase(removeWithDelay.fulfilled, (state, action) => {
+      state.count = action.payload;
+    });
 
-export default authSlice.reducer
-export const {add, remove, logout} = authSlice.actions
-export const authSelector = (state:RootState)=>state.authReducer
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isAuthented = true;
+      state.isError = false;
+      state.loginResult = action.payload;
+      state.isAuthenticating = false;
+    });
+  },
+});
+
+export default authSlice.reducer;
+export const { add, remove, logout } = authSlice.actions;
+export const authSelector = (state: RootState) => state.authReducer;
