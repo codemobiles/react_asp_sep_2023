979686759f710231bfa40ff22f0d610b114e807a
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-06 15:51:13 +0700

    1234

--- frontend/src/store/slices/stockSlice.ts
+++ frontend/src/store/slices/stockSlice.ts
@@ -1,7 +1,7 @@
 import { Product } from "@/types/product.type";
 import { httpClient } from "@/utils/HttpClient";
 import { server } from "@/utils/constants";
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export interface StockState {
     stockAllResult: Product[];
@@ -13,10 +13,10 @@ export interface StockState {
     stockOneResult: null,
   };
 
-export const getProducts = async ()=>{
+export const getProducts = createAsyncThunk("stock/getProducts", async ()=>{
     const result = await httpClient.get<Product[]>(server.PRODUCT_URL)
     return result.data
-} 
+}) 
   
 const stockSlice = createSlice({
     name:"stockSlice",
