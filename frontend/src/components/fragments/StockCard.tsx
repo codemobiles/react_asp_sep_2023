6daf8b0cd3b222975e10198241af8b2ceaf6504c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 11:30:28 +0700

    1234

--- frontend/src/components/fragments/StockCard.tsx
+++ frontend/src/components/fragments/StockCard.tsx
@@ -3,5 +3,33 @@ import React from "react";
 type Props = {};
 
 export default function StockCard({}: Props) {
-  return <div>StockCard</div>;
+  return (
+    <Card>
+      <Grid container className="min-h-[70px]">
+        <Grid item className="flex-grow h-[100px] p-2">
+          <Typography variant="h5" color="textPrimary">
+            {props.title}
+          </Typography>
+          <Typography variant="h6" color="textSecondary">
+            {props.subtitle}
+          </Typography>
+        </Grid>
+
+        <Grid
+          item
+          style={{
+            backgroundColor: props.color,
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            minWidth: 70,
+          }}
+        >
+          <props.icon fontSize="large" />
+          {/* {React.createElement(props.icon, { fontSize: "large" })} */}
+        </Grid>
+      </Grid>
+    </Card>
+  );
 }
