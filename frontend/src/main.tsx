1a8af39bf5af7bb303562ca33ed7db571c8a771e
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 10:28:55 +0700

    1234

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
+import InjectTailwind from "InjectTailwind.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* HOC - Higher order component */}
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <InjectTailwind>
+          <App />
+        </InjectTailwind>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
