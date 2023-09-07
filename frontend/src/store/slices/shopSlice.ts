922a677a756430f04bb5c0a7044393e56f622363
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 15:29:19 +0700

    1234

--- frontend/src/store/slices/shopSlice.ts
+++ frontend/src/store/slices/shopSlice.ts
@@ -1,9 +1,34 @@
+import { Product } from "@/types/product.type";
+import { TransactionResponse } from "@/types/transaction.type";
 import { createSlice } from "@reduxjs/toolkit";
+
+export interface ShopState {
+  transactionAllResult: TransactionResponse[];
+  mOrderLines: Product[];
+  mTotalPrice: number;
+  mTaxAmt: number;
+  mIsPaymentMade: boolean;
+  mGiven: number;
+}
+
+const initialState: ShopState = {
+  transactionAllResult: [],
+  mOrderLines: [],
+  mTotalPrice: 0,
+  mTaxAmt: 0,
+  mIsPaymentMade: false,
+  mGiven: 0,
+};
 
 const shopSlice = createSlice({
   name: "shopSlice",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    addOrder: () => {},
+    removeOrder: () => {},
+    shopSelector: () => {},
+    togglePayment: () => {},
+  },
 });
 
 export default shopSlice.reducer;
