63d5e56e2baed39bf511046575cda0a448307b0d
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 15:01:03 +0700

    1234

--- frontend/src/components/pages/StockEditPage.tsx
+++ frontend/src/components/pages/StockEditPage.tsx
@@ -1,10 +1,170 @@
-import React from "react";
-import { useMatch } from "react-router-dom";
+import { Box, Button, TextField } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
-type Props = {};
+import { Link, useNavigate } from "react-router-dom";
+import { addProduct } from "@/store/slices/stockSlice";
+import { useAppDispatch } from "@/store/store";
+import { Product } from "@/types/product.type";
 
-export default function StockEditPage({}: Props) {
-  const match = useMatch("/stock/edit/:id");
+import { useForm, Controller } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-  return <div>StockEditPage {match?.params.id}</div>;
-}
+const formValidateSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required").trim(),
+  price: Yup.number().min(100, "Number must be greater than 100"),
+  stock: Yup.number().min(100, "Number must be greater than 100"),
+});
+
+const StockEdit = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = async (values: Product) => {
+    const formData = new FormData();
+    formData.append("name", values.name);
+    formData.append("price", String(values.price));
+    formData.append("stock", String(values.stock));
+    formData.append("file", values.file);
+    const result = await dispatch(addProduct(formData));
+    if (addProduct.fulfilled.match(result)) {
+      navigate("/stock");
+    }
+  };
+
+  const initialValue: Product = { name: "Productx", price: 1500, stock: 9999 };
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<Product>({
+    defaultValues: initialValue,
+    //@ts-ignore
+    resolver: yupResolver(formValidateSchema),
+  });
+
+  const watchPreviewImage = watch("file_obj");
+
+  const showForm = () => {
+    return (
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
+        <Card elevation={5}>
+          <CardContent className="p-8">
+            <Typography gutterBottom variant="h3">
+              Edit Product
+            </Typography>
+
+            {/* Name */}
+            <Controller
+              control={control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <TextField
+                    {...field}
+                    label="Name"
+                    type="text"
+                    error={Boolean(errors.name?.message)}
+                    helperText={errors.name?.message?.toString()}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    autoFocus
+                  />
+                );
+              }}
+            />
+
+            <Controller
+              name="price"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <TextField
+                    {...field}
+                    label="Price"
+                    type="number"
+                    error={Boolean(errors.price?.message)}
+                    helperText={errors.price?.message?.toString()}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    autoFocus
+                  />
+                );
+              }}
+            />
+
+            <Controller
+              name="stock"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <TextField
+                    {...field}
+                    type="number"
+                    label="Stock"
+                    error={Boolean(errors.stock?.message)}
+                    helperText={errors.stock?.message?.toString()}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    autoFocus
+                  />
+                );
+              }}
+            />
+
+            <Box>{showPreviewImage()}</Box>
+
+            <TextField
+              className="mt-4"
+              type="file"
+              fullWidth
+              onChange={(e: React.ChangeEvent<any>) => {
+                e.preventDefault();
+                setValue("file", e.target.files[0]); // for upload
+                setValue("file_obj", URL.createObjectURL(e.target.files[0])); // for preview image
+              }}
+            />
+          </CardContent>
+          <CardActions>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+              className="mr-2"
+            >
+              Create
+            </Button>
+            <Button
+              fullWidth
+              component={Link}
+              to="/stock"
+              color="info"
+              variant="outlined"
+            >
+              Cancel
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+    );
+  };
+
+  const showPreviewImage = () => {
+    if (watchPreviewImage) {
+      return <img alt="" src={watchPreviewImage} className="h-[100px]" />;
+    }
+  };
+
+  return <Box>{showForm()}</Box>;
+};
+
+export default StockEdit;
