6b2bedce2ca5e5fb99e3a5fafac2ed1c828a686d
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 10:04:03 +0700

    1234

--- frontend/src/components/pages/StockPage.tsx
+++ frontend/src/components/pages/StockPage.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
 import { useAppDispatch } from "@/store/store";
 import { useSelector } from "react-redux";
 import { authSelector } from "@/store/slices/authSlice";
 import { getProducts, stockSelector } from "@/store/slices/stockSlice";
+import { Typography } from "@mui/material";
+import { NumericFormat } from "react-number-format";
 
 const columns: GridColDef[] = [
   { field: "productId", headerName: "Id", width: 70 },
@@ -23,7 +30,23 @@ const columns: GridColDef[] = [
   },
   { field: "name", headerName: "Name", width: 430 },
   { field: "price", headerName: "Price", width: 130 },
-  { field: "stock", headerName: "Stock", width: 130 },
+  {
+    field: "stock",
+    headerName: "Stock",
+    width: 130,
+    renderCell: ({ value }: GridRenderCellParams) => (
+      <Typography variant="body1">
+        <NumericFormat
+          value={value}
+          displayType={"text"}
+          thousandSeparator={true}
+          decimalScale={0}
+          fixedDecimalScale={true}
+        />
+      </Typography>
+    ),
+  },
+  { field: "created", headerName: "Created", width: 230 },
 ];
 
 export default function StockPage() {
