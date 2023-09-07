c718fa7fc45ab09d565f9eac15fa9813bdff36c8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 10:30:58 +0700

    1234

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
-import InjectTailwind from "InjectTailwind.tsx";
+import InjectTailwind from "./InjectTailwind.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
