cb931a86ee5e3ed1afa70c632e8ba30a9954a1ca
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 15:25:49 +0700

    1234

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,22 +1,22 @@
-import {  configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 
 import authReducer from "@/store/slices/authSlice";
 import stockReducer from "@/store/slices/stockSlice";
+import shopReducer from "@/store/slices/shopSlice";
 
 // let store: any = undefined;
-const store  = configureStore({
+const store = configureStore({
   reducer: {
     authReducer,
-    stockReducer
+    stockReducer,
+    shopReducer,
   },
   devTools: import.meta.env.VITE_IS_PRODUCTION === "0", // show redux log in dev mode
 });
-
 
 // export type of root state from reducers
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export default store;
-
