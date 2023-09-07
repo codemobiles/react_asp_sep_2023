284785bb52ce316ea98ea166c5fa9dc8d9be7774
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-07 09:35:26 +0700

    1234

--- frontend/src/components/pages/StockPage.tsx
+++ frontend/src/components/pages/StockPage.tsx
@@ -2,21 +2,16 @@ import * as React from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "id", headerName: "Id", width: 70 },
+  { field: "name", headerName: "Name", width: 330 },
+  { field: "price", headerName: "Price", width: 130 },
+  { field: "stock", headerName: "Stock", width: 130 },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, name: "Product1", price: 10, stock: 345 },
+  { id: 2, name: "Product2", price: 20, stock: 235 },
+  { id: 3, name: "Product3", price: 30, stock: 135 },
 ];
 
 export default function StockPage() {
