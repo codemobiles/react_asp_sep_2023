0fb93623540bf494d44ad7a58f6751438d0b6e44
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-08 10:58:38 +0700

    1234

--- frontend/src/components/pages/TransactionPage.tsx
+++ frontend/src/components/pages/TransactionPage.tsx
@@ -1,66 +1,144 @@
-import * as React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-import { useAppDispatch } from "@/store/store";
-import { useSelector } from "react-redux";
 import { getTransactions, shopSelector } from "@/store/slices/shopSlice";
+import { useAppDispatch } from "@/store/store";
+import { imageUrl } from "@/utils/constants";
+import { Avatar, Grid, Paper, Stack, Typography } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridRowId,
+} from "@mui/x-data-grid";
 import dayjs from "dayjs";
 import "dayjs/locale/th";
+import { useEffect, useState } from "react";
 import { NumericFormat } from "react-number-format";
-import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
-const columns: GridColDef[] = [
-  { field: "transactionId", headerName: "ID", width: 70 },
-  {
-    field: "timestamp",
-    headerName: "Time",
-    width: 130,
-    renderCell({ value }) {
-      return dayjs(value).locale("th").add(543, "year").format("DD MMMM YYYY");
-    },
-  },
-  {
-    field: "total",
-    headerName: "Total",
-    width: 130,
-    renderCell({ value }) {
-      return (
-        <Typography variant="body1">
-          <NumericFormat
-            value={value}
-            displayType={"text"}
-            thousandSeparator={true}
-            decimalScale={0}
-            fixedDecimalScale={true}
-            prefix={"฿"}
-          />
-        </Typography>
-      );
-    },
-  },
-];
-
-export default function TransactionPage() {
+const Transaction = () => {
   const dispatch = useAppDispatch();
+  const [orderList, setOrderList] = useState([]);
+  const [selectedId, setSelectedId] = useState<GridRowId>(0);
+
   const shopReducer = useSelector(shopSelector);
 
-  React.useEffect(() => {
+  useEffect(() => {
     dispatch(getTransactions());
   }, [dispatch]);
 
+  const transactionColumns: GridColDef[] = [
+    {
+      headerName: "ID",
+      field: "transactionId",
+      width: 50,
+    },
+
+    {
+      headerName: "DATE",
+      field: "timestamp",
+      width: 150,
+      renderCell: (params: GridRenderCellParams) => (
+        <Typography variant="body1">
+          {/* 543 diff thai years */}
+          {dayjs(params.value)
+            .locale("th")
+            .add(543, "year")
+            .format("DD MMMM YYYY")}
+        </Typography>
+      ),
+    },
+    {
+      headerName: "STAFF",
+      width: 120,
+      field: "employeeId",
+    },
+    {
+      headerName: "TOTAL",
+      field: "total",
+      width: 70,
+      renderCell: (params: GridRenderCellParams) => (
+        <NumericFormat
+          value={params.value}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"฿"}
+        />
+      ),
+    },
+    {
+      headerName: "PAID",
+      field: "paid",
+      width: 70,
+      renderCell: (params: GridRenderCellParams) => (
+        <NumericFormat
+          value={params.value}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"฿"}
+        />
+      ),
+    },
+    {
+      headerName: "IMG",
+      width: 50,
+      field: "orderList",
+      renderCell: (params: GridRenderCellParams<any>) => {
+        try {
+          const orderList = JSON.parse(params.value);
+          return <Avatar src={`${imageUrl}//${orderList[0].image}`} />;
+        } catch (e) {
+          return <Typography>N/A</Typography>;
+        }
+      },
+    },
+  ];
+
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        getRowId={(item) => item.transactionId}
-        rows={shopReducer.transactionAllResult}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
+    <Paper className="p-8">
+      <Grid container spacing={2} className="h-[80vh]">
+        <Grid item xs={orderList.length ? 7 : 12}>
+          <DataGrid
+            sx={{
+              height: "70vh",
+              "& .MuiDataGrid-cell:focus": { outline: "solid #2196f3 0px" },
+              "& .MuiDataGrid-row:nth-of-type(even)": {
+                backgroundColor: "#f5f5f5",
+                "&:hover": {
+                  backgroundColor: "#e0f7fa !important",
+                },
+              },
+              "& .MuiDataGrid-row:nth-of-type(odd)": {
+                backgroundColor: "#ffffff",
+                "&:hover": {
+                  backgroundColor: "#e0f7fa !important",
+                },
+              },
+            }}
+            getRowId={(row: any) => row.transactionId}
+            onRowSelectionModelChange={(newSelectionModel: any) => {
+              setSelectedId(newSelectionModel[0]);
+            }}
+            rowSelectionModel={[selectedId]}
+            onRowClick={(e) => {
+              setOrderList(JSON.parse(e.row.orderList));
+            }}
+            rows={shopReducer.transactionAllResult}
+            columns={transactionColumns}
+            pageSizeOptions={[5, 10, 20, 100]}
+          />
+        </Grid>
+        <Grid item xs={orderList.length ? 5 : 0}>
+          <ul>
+            {orderList.map((item: any) => (
+              <Stack direction="row" spacing={1}>
+                <Avatar src={`${imageUrl}//${item.image}`} />
+                <li>{item.name}</li>
+              </Stack>
+            ))}
+          </ul>
+        </Grid>
+      </Grid>
+    </Paper>
   );
-}
+};
+
+export default Transaction;
