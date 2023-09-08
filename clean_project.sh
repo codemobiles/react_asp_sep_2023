9de8dbab3f7021d5a02ee0e0fbbce04efed93a85
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-08 14:20:27 +0700

    1234

---
+++ clean_project.sh
@@ -0,0 +1,9 @@
+# backend .net
+rm -Rf /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/backend/bin
+rm -Rf /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/backend/obj
+rm -f /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/backend/Logs/*.*
+
+# frontend angular
+rm -Rf /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/frontend/.angular
+rm -Rf /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/frontend/node_modules
+rm -Rf /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/frontend/package-lock.json
---
+++ restore_project.sh
@@ -0,0 +1,9 @@
+# backend .net
+cd /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/backend && dotnet restore && dotnet build
+
+# frontend angular
+cd /Users/chaiyasittayabovorn/Desktop/Training/DotNetCore_Angular/angular_dotnetcore_2023/frontend && yarn
+
+
+# restore database
+checkout this file in backend/Readme/readme_db.md
