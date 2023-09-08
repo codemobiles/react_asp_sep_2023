d0b6890ca4f5275081c52072b306b48705919082
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-08 15:43:37 +0700

    1234

---
+++ backend/Controllers/v2/AuthController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// using backend.Models;
+using backend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using backend.Models;
+using backend.Services;
+
+namespace backend.Controllers.v2
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMapper _autoMapper;
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IMapper autoMapper, IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+            _autoMapper = autoMapper;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginViewModel userViewModel)
+        {
+            try
+            {
+                // mapper tutorial - readme_mapper.md
+                var user = _autoMapper.Map<User>(userViewModel);
+                (User? result, string token) = _authRepository.Login(user);
+
+                if (result == null)
+                {
+                    return Unauthorized(new { token = "", message = "username invalid" });
+                }
+
+                if (String.IsNullOrEmpty(token))
+                {
+                    return Unauthorized(new { token = "", message = "password invalid" });
+                }
+
+                return Ok(new { token = token, message = "login successfully", result = "ok" });
+            }
+            catch (Exception error)
+            {
+
+                return StatusCode(500, new { message = error });
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterViewModel viewmodel)
+        {
+            var user = _autoMapper.Map<User>(viewmodel);
+            _authRepository.Register(user);
+            return Ok();
+        }
+
+    }
+}
